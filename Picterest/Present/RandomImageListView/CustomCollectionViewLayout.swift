c52488d67d3a620bae733e5629a430b682c33e92
kimkirim <kirimrlfla@naver.com>
2022-07-25 18:49:03 +0900

    [feat] CustomCollectionViewLayout 구현 #8

---
+++ Picterest/Present/RandomImageListView/CustomCollectionViewLayout.swift
@@ -0,0 +1,81 @@
+//
+//  CustomCollectionViewLayout.swift
+//  Picterest
+//
+//  Created by 김기림 on 2022/07/25.
+//
+
+import UIKit
+
+class CustomCollectionViewLayout: UICollectionViewLayout {
+    weak var delegate: CustomCollectionViewLayoutDelegate!
+
+    fileprivate var cellPadding: CGFloat = 6
+
+    fileprivate var cache = [UICollectionViewLayoutAttributes]()
+
+    fileprivate var contentHeight: CGFloat = 0
+
+    fileprivate var contentWidth: CGFloat {
+      guard let collectionView = collectionView else {
+        return 0
+      }
+      let insets = collectionView.contentInset
+        return collectionView.bounds.width - (insets.left + insets.right)
+    }
+    
+    override var collectionViewContentSize: CGSize {
+        return CGSize(width: contentWidth, height: contentHeight)
+    }
+    
+    override func prepare() {
+        super.prepare()
+        guard cache.isEmpty == true, let collectionView = collectionView else {
+            return
+        }
+        for section in 0 ..< collectionView.numberOfSections {
+            let numberOfColumns = delegate?.collectionView(collectionView, numberOfColumnsInSection: section) ?? 1
+            let columnWidth = contentWidth / CGFloat(numberOfColumns)
+            var xOffset = [CGFloat]()
+            for column in 0 ..< numberOfColumns {
+                xOffset.append(CGFloat(column) * columnWidth)
+            }
+            var column = 0
+            var yOffset = [CGFloat](repeating: 0, count: numberOfColumns)
+            
+            for item in 0 ..< collectionView.numberOfItems(inSection: section) {
+                let indexPath = IndexPath(item: item, section: 0)
+                
+                let photoHeight = delegate.collectionView(collectionView, heightForPhotoAtIndexPath: indexPath)
+                let height = cellPadding * 2 + photoHeight
+                let frame = CGRect(x: xOffset[column], y: yOffset[column], width: columnWidth, height: height)
+                let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
+                
+                let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+                attributes.frame = insetFrame
+                cache.append(attributes)
+                
+                contentHeight = max(contentHeight, frame.maxY)
+                yOffset[column] = yOffset[column] + height
+                
+                column = column < (numberOfColumns - 1) ? (column + 1) : 0
+            }
+        }
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var visibleLayoutAttributes = [UICollectionViewLayoutAttributes]()
+        
+        // Loop through the cache and look for items in the rect
+        for attributes in cache {
+            if attributes.frame.intersects(rect) {
+                visibleLayoutAttributes.append(attributes)
+            }
+        }
+        return visibleLayoutAttributes
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return cache[indexPath.item]
+    }
+}
