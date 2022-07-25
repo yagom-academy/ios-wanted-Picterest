a779fa4288ec2f10c2feea81b5a4c1e5b1d46f6f
iclxxud <107861353+iclxxud@users.noreply.github.com>
2022-07-26 00:25:57 +0900

    [feat] ImagesView 컬렉션뷰 기본 세팅 구현 #3

--- Picterest/Presentation/ImagesViewController.swift
+++ Picterest/Presentation/ImagesViewController.swift
@@ -8,13 +8,25 @@ import UIKit
 class ImagesViewController: UIViewController {
     
     // MARK: - Properties
-    var imageDatas: [ImageInfo] = []
+    private var imageDatas: [ImageInfo] = []
     
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .blue
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchData()
+        configureUI()
     }
+}
 
+extension ImagesViewController {
     
     private func fetchData() {
         NetworkManager.shard.fetchImages { result in
@@ -26,6 +38,28 @@ class ImagesViewController: UIViewController {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    private func configureUI() {
+        
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            ])
+    }
+}
+
+// MARK: - CollectionView DataSoucrce
+extension ImagesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        imageDatas.count
     }
 }
 
