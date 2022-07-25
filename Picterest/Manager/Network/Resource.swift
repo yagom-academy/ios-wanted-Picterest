d0dbc203a28fa61bf9847f5e4716795a488d4ad2
jmindevelper <jmin.developer@gmail.com>
2022-07-25 14:09:31 +0900

    [feat] request생성을 위한 Resource 객체 생성

---
+++ Picterest/Manager/Network/Resource.swift
@@ -0,0 +1,43 @@
+//
+//  Resource.swift
+//  Picterest
+//
+//  Created by J_Min on 2022/07/25.
+//
+
+import Foundation
+
+struct Resource<T> {
+    var base: String
+    var path: String
+    var params: [String: String]
+    var header: [String: String]
+    
+    var urlRequest: URLRequest? {
+        guard var urlComponents = URLComponents(string: base + path) else { return nil }
+        let queryItems = params.map {
+            URLQueryItem(name: $0.key, value: $0.value)
+        }
+        urlComponents.queryItems = queryItems
+        
+        guard let url = urlComponents.url else { return nil }
+        var request = URLRequest(url: url)
+        header.forEach {
+            request.addValue($0.key, forHTTPHeaderField: $0.value)
+        }
+        
+        return request
+    }
+    
+    init(
+        base: String = "https://api.unsplash.com",
+        path: String = "/photos/random",
+        params: [String: String] = ["count": "15"],
+        header: [String: String] = ["Authorization": "Client-ID \(APIKey.shared.accessKey)"]
+    ) {
+        self.base = base
+        self.path = path
+        self.params = params
+        self.header = header 
+    }
+}
