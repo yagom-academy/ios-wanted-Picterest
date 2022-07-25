9cea6f5dd73bc217c9aac31b1beb8284bae35bf2
신의연 <dusdl14@gmail.com>
2022-07-25 17:09:49 +0900

    [feat] Network EndPoint 구성 #1

--- Picterest/Network/ImageEndPoint.swift
+++ Picterest/Network/ImageEndPoint.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+protocol ImageEndPointType {
+    var baseUrl: String { get }
+    var accessKey: String { get }
+    var method: HttpMethod { get }
+    
+}
+
+enum ImageEndPoint: ImageEndPointType {
+    
+    case getImage
+    
+    var baseUrl: String {
+        switch self {
+        case .getImage:
+            return "https://api.unsplash.com/photos/random"
+        }
+        
+    }
+    
+    var accessKey: String {
+        switch self {
+        case .getImage:
+            return "r6IAzQw2BwX75Nx0Y9ACzqM1yc1MdVEvNavR2jnZ-Wc"
+        }
+        
+    }
+    
+    var method: HttpMethod {
+        switch self {
+        case .getImage:
+            return .get
+        }
+        
+    }
+    
+}
