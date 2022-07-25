8fbd5481abdbfb5fbecb41cf70d46b267d29f5b5
신의연 <dusdl14@gmail.com>
2022-07-25 22:27:04 +0900

    [feat] VM에서 Image resizing 완료

--- Picterest/ViewModel/ImageListViewModel.swift
+++ Picterest/ViewModel/ImageListViewModel.swift
@@ -6,11 +6,12 @@
 //
 
 import Foundation
-
+import UIKit
 class ImageListViewModel {
     
     var repository = Repository()
     var imageList = [ImageData]()
+    var imageSizeList = [CGFloat]()
     
     private var loading = false
     
@@ -27,19 +28,17 @@ class ImageListViewModel {
         return imageList[index]
     }
     
+    func imageSize(at index: Int) -> CGFloat {
+        return imageSizeList[index]
+    }
+    
     func list() {
         loading = true
         loadingStarted()
-        repository.fetchImage { result in
-            switch result {
-            case .success(let imagelist):
-                self.imageList = imagelist
-                self.imageListUpdate()
-                self.loadingEnded()
-                self.loading = false
-            case .failure(let error):
-                print(error)
-            }
+        resizingImage {
+            self.imageListUpdate()
+            self.loadingEnded()
+            self.loading = false
         }
     }
     
@@ -55,6 +54,27 @@ class ImageListViewModel {
 //            self.loadingEnded()
 //            self.loading = false
 //        }
+    }
+    
+    func resizingImage(completion: @escaping () -> Void) -> Void {
+        repository.fetchImage { [self] result in
+            switch result {
+            case .success(let imagelist):
+                self.imageList = imagelist
+                imageList.forEach {
+                    let height = getImageHeight(height: CGFloat($0.height), width: CGFloat($0.width))
+                    imageSizeList.append(height)
+                }
+                completion()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getImageHeight(height: CGFloat, width: CGFloat) -> CGFloat {
+        let scale = 150 / width
+        return height * scale
     }
     
 }
