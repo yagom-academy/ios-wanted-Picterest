d90748007f532c1797a91dbd10ea16332ee5073d
이경민 <cow970814@naver.com>
2022-07-25 13:40:36 +0900

    feat : 탭바 컨트롤러 설정

---
+++ Picterest/Views/FeedView/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  Picterest
+//
+//  Created by 이경민 on 2022/07/25.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
