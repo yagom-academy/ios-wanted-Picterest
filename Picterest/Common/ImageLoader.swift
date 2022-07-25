e43e7d5ea74ec5e67310d6c5363d9e1c15d55814
prettyturtle <unifconti@gmail.com>
2022-07-26 01:22:15 +0900

    feat: ImageLoader 구현

--- Picterest/Common/ImageLoader.swift
+++ Picterest/Common/ImageLoader.swift
@@ -5,4 +5,43 @@
 //  Created by yc on 2022/07/26.
 //
 
-import Foundation
+import UIKit
+
+enum ImageLoaderError: Error {
+    case unknown
+}
+
+class ImageLoader {
+    static let shared = ImageLoader()
+    private init() {}
+    
+    func downloadImage(
+        urlString: String,
+        completion: @escaping (Result<UIImage, ImageLoaderError>) -> Void
+    ) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.unknown))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.unknown))
+                return
+            }
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                completion(.failure(.unknown))
+                return
+            }
+            
+            guard let data = data,
+                  let image = UIImage(data: data) else {
+                completion(.failure(.unknown))
+                return
+            }
+            
+            completion(.success(image))
+        }.resume()
+    }
+}
