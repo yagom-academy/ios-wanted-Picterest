b5585b8940939e0ddb014045cd054b94ec7026ca
신의연 <dusdl14@gmail.com>
2022-07-25 19:42:21 +0900

    [feat] ImageListVC 구현 #10

--- Picterest/Views/ImageList/ImageListViewController.swift
+++ Picterest/Views/ImageList/ImageListViewController.swift
@@ -9,9 +9,79 @@ import UIKit
 
 class ImageListViewController: UIViewController {
 
+    var photos = [ImageData]()
+    
+    private var picterestCollectionView: UICollectionView = {
+        let layout = PicterestCollectionViewLayout()
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(PicterestCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDelegate()
+        setLayout()
+        fetchPicterestData()
+    }
+    
+    func setDelegate() {
+        picterestCollectionView.delegate = self
+        picterestCollectionView.dataSource = self
+        if let layout = picterestCollectionView.collectionViewLayout as? PicterestCollectionViewLayout {
+            layout.delegate = self
+        }
+    }
+    
+    func setLayout() {
         view.backgroundColor = .systemBackground
+        view.addSubview(picterestCollectionView)
+        
+        NSLayoutConstraint.activate([
+            picterestCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            picterestCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            picterestCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            picterestCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+    
+    func fetchPicterestData() {
+        Repository().fetchImage { [self] result in
+            switch result {
+            case .success(let imageData):
+                photos = imageData
+                print(imageData)
+                DispatchQueue.main.async { [self] in
+                    picterestCollectionView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
+}
+
+extension ImageListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(photos.count)
+        return photos.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = picterestCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PicterestCollectionViewCell
+        cell.fetchImageData(data: photos[indexPath.row])
+        return cell
+    }
+}
+
+extension ImageListViewController: PinterestLayoutDelegate {
+  func collectionView(_ collectionView: UICollectionView,
+                      heightForPhotoAtIndexPath indexPath:IndexPath) -> CGFloat {
+    // image view size 조절 클래스 필요
+      //return photos[indexPath.item].image.size.height
+      return 200
+  }
 }
