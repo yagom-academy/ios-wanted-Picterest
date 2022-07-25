c3b88d43a7a55bbdcee3d49e5b74b70bfe1bd514
wooooozin <wooooozin@gmail.com>
2022-07-25 14:44:42 +0900

    [feat] PhotoListCollectionViewLayout 설정 및 구현

--- Picterest/Source/PhotoList/PhotoListCell/PhotoListCollectionViewCell.swift
+++ Picterest/Source/PhotoList/PhotoListCell/PhotoListCollectionViewCell.swift
@@ -9,10 +9,12 @@ import UIKit
 class PhotoListCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var radomImageView: UIImageView!
+    @IBOutlet weak var savedButton: UIButton!
+    @IBOutlet weak var captionLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        contentView.layer.cornerRadius = 10
+        contentView.layer.masksToBounds = true
     }
-
 }
