0d81bde506d2cb9d2f73a2560892f6a6ff967afe
jmindevelper <jmin.developer@gmail.com>
2022-07-25 15:03:16 +0900

    [feat] RandomImageViewController CollectionView 기본세팅
    - layout CustomLayout으로 바꿔야함

--- Picterest/Controller/RandomImageViewController.swift
+++ Picterest/Controller/RandomImageViewController.swift
@@ -9,8 +9,54 @@ import UIKit
 
 final class RandomImageViewController: UIViewController {
     
+    // MARK: - ViewProperties
+    private lazy var randomImageCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .yellow
+        collectionView.dataSource = self
+        
+        return collectionView
+    }()
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigation()
+        configureSubView()
+        setConfigurationsOfRandomImageCollectionView()
+    }
+}
+
+// MARK: - UI
+extension RandomImageViewController {
+    private func configureNavigation() {
+        navigationItem.title = "Random Image"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureSubView() {
+        view.addSubview(randomImageCollectionView)
+    }
+    
+    private func setConfigurationsOfRandomImageCollectionView() {
+        NSLayoutConstraint.activate([
+            randomImageCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            randomImageCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            randomImageCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            randomImageCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension RandomImageViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
 }
