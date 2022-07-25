a88e6e2b190ec857b1d1cb629057f0bee52ecc2a
신의연 <dusdl14@gmail.com>
2022-07-25 17:49:53 +0900

    [feat] storyboard 삭제

---
+++ Picterest/Views/ImageTabbarController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageTabbarController.swift
+//  Picterest
+//
+//  Created by 신의연 on 2022/07/25.
+//
+
+import UIKit
+
+class ImageTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
