60f0eff9b1b062f753297f693b0b33deba27b4b8
wooooozin <wooooozin@gmail.com>
2022-07-25 12:49:21 +0900

    [feat] TabBar Controller 화면 구성

---
+++ Picterest/Source/PhotoList/PhotoListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoListViewController.swift
+//  Picterest
+//
+//  Created by 효우 on 2022/07/25.
+//
+
+import UIKit
+
+class PhotoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Picterest/Source/PhotoSave/PhotoSaveViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoSaveViewController.swift
+//  Picterest
+//
+//  Created by 효우 on 2022/07/25.
+//
+
+import UIKit
+
+class PhotoSaveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
