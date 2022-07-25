8fe83fd9731c5504cca42dcf91bd3fb2eb63d052
신의연 <dusdl14@gmail.com>
2022-07-25 19:40:55 +0900

    [chore] 기능별 폴더 변경

---
+++ Picterest/Views/ImageList/PicterestCollectionViewLayout.swift
@@ -0,0 +1,12 @@
+//
+//  PicterestCollectionViewLayout.swift
+//  Picterest
+//
+//  Created by 신의연 on 2022/07/25.
+//
+
+import UIKit
+
+class PicterestCollectionViewLayout: UICollectionViewLayout {
+
+}
