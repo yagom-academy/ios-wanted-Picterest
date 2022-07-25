21c4b91bca508c2d0666e7400f178163342317e6
hhhan0315 <hhan0315@naver.com>
2022-07-25 15:04:41 +0900

    <feat> MainTabBarController 구현 - PhotosViewController - SavedViewController

--- Picterest/ViewController/MainTabBarController.swift
+++ Picterest/ViewController/MainTabBarController.swift
@@ -9,4 +9,53 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
+    enum MainTabBarItem {
+        case photo
+        case saved
+        
+        var title: String {
+            switch self {
+            case .photo:
+                return "Photos"
+            case .saved:
+                return "Saved"
+            }
+        }
+        
+        var image: UIImage? {
+            switch self {
+            case .photo:
+                return UIImage(systemName: "photo.fill")
+            case .saved:
+                return UIImage(systemName: "star.fill")
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+    }
+}
+
+// MARK: - Private
+
+extension MainTabBarController {
+    private func configure() {
+        configureUI()
+    }
+    
+    private func configureUI() {
+        let photosTabBarItem = UITabBarItem(title: MainTabBarItem.photo.title, image: MainTabBarItem.photo.image, selectedImage: nil)
+        let savedTabBarItem = UITabBarItem(title: MainTabBarItem.saved.title, image: MainTabBarItem.saved.image, selectedImage: nil)
+        
+        let photosViewController = PhotosViewController()
+        photosViewController.tabBarItem = photosTabBarItem
+        
+        let savedViewController = SavedViewController()
+        savedViewController.tabBarItem = savedTabBarItem
+        
+        viewControllers = [photosViewController, savedViewController]
+    }
 }
---
+++ Picterest/ViewController/SavedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SavedViewController.swift
+//  Picterest
+//
+//  Created by rae on 2022/07/25.
+//
+
+import UIKit
+
+class SavedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        view.backgroundColor = .orange
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
