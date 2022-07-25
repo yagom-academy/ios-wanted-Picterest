0ea2c641fbfae4e32b7defc5d3a29f7856659cb6
jmindevelper <jmin.developer@gmail.com>
2022-07-25 13:50:25 +0900

    [feat] APIKey를 가지고있는 싱글톤 객체 생성

---
+++ Picterest/Application/APIKey.swift
@@ -0,0 +1,17 @@
+//
+//  APIKey.swift
+//  Picterest
+//
+//  Created by J_Min on 2022/07/25.
+//
+
+import Foundation
+
+class APIKey {
+    static let shared = APIKey()
+    
+    let accessKey = "Hv8wpp9ZIbUOAM84jdQXC6GTBXcRIUovZq2ehkfu0CQ"
+    let secretKey = "K_iuw1Ck389YbTbcdSKeJGIlTrn3En7HLSA98M7hp9o"
+    
+    private init() { }
+}
