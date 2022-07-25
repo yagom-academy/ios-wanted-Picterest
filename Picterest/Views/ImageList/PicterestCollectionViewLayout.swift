60bf5d99825f120ab4cfcc98560cdf869bea1859
신의연 <dusdl14@gmail.com>
2022-07-25 20:32:12 +0900

    [refact] 코드 컨벤션

--- Picterest/Views/ImageList/PicterestCollectionViewLayout.swift
+++ Picterest/Views/ImageList/PicterestCollectionViewLayout.swift
@@ -14,15 +14,12 @@ protocol PinterestLayoutDelegate: AnyObject {
 class PicterestCollectionViewLayout: UICollectionViewLayout {
 
     weak var delegate: PinterestLayoutDelegate!
-
-    // 2
+    
     fileprivate var numberOfColumns = 2
     fileprivate var cellPadding: CGFloat = 6
 
-    // 3
     fileprivate var cache = [UICollectionViewLayoutAttributes]()
 
-    // 4
     fileprivate var contentHeight: CGFloat = 0
 
     fileprivate var contentWidth: CGFloat {
@@ -33,7 +30,6 @@ class PicterestCollectionViewLayout: UICollectionViewLayout {
       return collectionView.bounds.width - (insets.left + insets.right)
     }
 
-    // 5
     override var collectionViewContentSize: CGSize {
       return CGSize(width: contentWidth, height: contentHeight)
     }
@@ -42,7 +38,6 @@ class PicterestCollectionViewLayout: UICollectionViewLayout {
         guard cache.isEmpty == true, let collectionView = collectionView else {
            return
          }
-         // 2
          let columnWidth = contentWidth / CGFloat(numberOfColumns)
          var xOffset = [CGFloat]()
          for column in 0 ..< numberOfColumns {
@@ -51,23 +46,19 @@ class PicterestCollectionViewLayout: UICollectionViewLayout {
          var column = 0
          var yOffset = [CGFloat](repeating: 0, count: numberOfColumns)
            
-         // 3
          for item in 0 ..< collectionView.numberOfItems(inSection: 0) {
              
            let indexPath = IndexPath(item: item, section: 0)
              
-           // 4
            let photoHeight = delegate.collectionView(collectionView, heightForPhotoAtIndexPath: indexPath)
            let height = cellPadding * 2 + photoHeight
            let frame = CGRect(x: xOffset[column], y: yOffset[column], width: columnWidth, height: height)
            let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
              
-           // 5
            let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
            attributes.frame = insetFrame
            cache.append(attributes)
              
-           // 6
            contentHeight = max(contentHeight, frame.maxY)
            yOffset[column] = yOffset[column] + height
              
@@ -84,8 +75,7 @@ class PicterestCollectionViewLayout: UICollectionViewLayout {
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
       
       var visibleLayoutAttributes = [UICollectionViewLayoutAttributes]()
-      
-      // Loop through the cache and look for items in the rect
+        
       for attributes in cache {
         if attributes.frame.intersects(rect) {
           visibleLayoutAttributes.append(attributes)
