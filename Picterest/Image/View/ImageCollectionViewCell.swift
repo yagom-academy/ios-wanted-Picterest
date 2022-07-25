ba1a8b258ec137e14a0bd9f58e21657b0dc0cf3b
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-07-25 16:08:46 +0900

    [feat] load image cache 구현

--- Picterest/Image/View/ImageCollectionViewCell.swift
+++ Picterest/Image/View/ImageCollectionViewCell.swift
@@ -8,5 +8,15 @@
 import UIKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
+    
     static let identifier = "ImageCollectionViewCell"
+    
+    private let photoImageView: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = 30
+        image.clipsToBounds = true
+        image.tintColor = .lightGray
+        
+        return image
+    }()
 }
