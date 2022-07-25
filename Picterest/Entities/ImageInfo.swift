aa049af2072441834665fe9c2a65e286d7daaa2e
kimkirim <kirimrlfla@naver.com>
2022-07-25 13:26:53 +0900

    [feat] ImageInfo 네트워크Get타입 구현 #6

--- Picterest/Entities/ImageInfo.swift
+++ Picterest/Entities/ImageInfo.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+struct ImageInfo: Decodable {
+    var updatedAt: String
+    var imageURL: ImageURL
+    var author: Author
+    var likes: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case updatedAt = "updated_at"
+        case imageURL = "urls"
+        case author = "user"
+        case likes
+    }
+}
+
+struct ImageURL: Decodable {
+    var thumbnail: String
+    var small: String
+    var medium: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case thumbnail = "thumb"
+        case small
+        case medium = "regular"
+    }
+}
+
+struct Author: Decodable {
+    var username: String
+    var name: String
+    var portfolioURL: String
+    var profileImage: Profile
+    
+    private enum CodingKeys: String, CodingKey {
+        case username, name
+        case portfolioURL = "portfolio_url"
+        case profileImage = "profile_image"
+    }
+}
+
+struct Profile: Decodable {
+    var url: String
+    private enum CodingKeys: String, CodingKey {
+        case url = "medium"
+    }
+}
