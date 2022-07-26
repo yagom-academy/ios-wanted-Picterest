f3bfd65bb04d7ca569b9821aae28a8a0d51c20be
신의연 <dusdl14@gmail.com>
2022-07-26 15:43:32 +0900

    [feat] PiceterstCollectionView Cell Savable delegate 로직 추가 #10

--- Picterest/Views/ImageList/ImageListViewController.swift
+++ Picterest/Views/ImageList/ImageListViewController.swift
@@ -90,6 +90,7 @@ extension ImageListViewController: UICollectionViewDelegate, UICollectionViewDat
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = picterestCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PicterestCollectionViewCell
+        cell.delegate = self
         let imageData = viewModel.image(at: indexPath.row)
         cell.fetchImageData(data: imageData, at: indexPath.row)
         return cell
@@ -110,4 +111,9 @@ extension ImageListViewController: PinterestLayoutDelegate {
     }
 }
 
+extension ImageListViewController: PicterestPhotoSavable {
+    func picterestCollectoinViewCell(imageData: ImageData) {
+        
+    }
+}
 
