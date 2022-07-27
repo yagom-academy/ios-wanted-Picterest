60f6fbb126f54543644fdc9974a01a4d052250c6
wooooozin <wooooozin@gmail.com>
2022-07-27 16:20:53 +0900

    [feat] PhotoSaveView UI 구현

---
+++ Picterest/Source/PhotoSave/PhotoSaveCell/PhotoSaveCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoSaveCollectionViewCell.swift
+//  Picterest
+//
+//  Created by 효우 on 2022/07/27.
+//
+
+import UIKit
+
+class PhotoSaveCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var savedButton: UIButton!
+    @IBOutlet weak var savedImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
--- Picterest/Source/PhotoSave/PhotoSaveViewController.swift
+++ Picterest/Source/PhotoSave/PhotoSaveViewController.swift
@@ -7,9 +7,58 @@
 import UIKit
 
 class PhotoSaveViewController: UIViewController {
-
+    @IBOutlet weak var saveListCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCollectionView()
+    }
+}
 
+//MARK: - Extension: CollectionView
+
+extension PhotoSaveViewController {
+    private func setCollectionView() {
+        saveListCollectionView.dataSource = self
+        saveListCollectionView?.contentInset = UIEdgeInsets(
+            top: 10,
+            left: 16,
+            bottom: 10,
+            right: 16
+        )
+        saveListCollectionView.register(
+            UINib(
+                nibName: "PhotoSaveCollectionViewCell",
+                bundle: nil
+            ),
+            forCellWithReuseIdentifier: "PhotoSaveCollectionViewCell"
+        )
+        let flowlayout = UICollectionViewFlowLayout()
+        let width: CGFloat = UIScreen.main.bounds.width - 32
+        let height: CGFloat = 300
+        flowlayout.itemSize = CGSize(width: width, height: height)
+        saveListCollectionView.collectionViewLayout = flowlayout
+    }
+}
+
+extension PhotoSaveViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 10
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let saveCell = saveListCollectionView.dequeueReusableCell(
+            withReuseIdentifier: "PhotoSaveCollectionViewCell",
+            for: indexPath
+        ) as? PhotoSaveCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return saveCell
     }
 }
