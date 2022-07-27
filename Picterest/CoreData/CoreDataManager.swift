f3dc7c1534ade2f6ef99f417a717e8cd866116fb
신의연 <dusdl14@gmail.com>
2022-07-27 10:29:37 +0900

    [feat] CoreaData와 FileManager 추가 #5 #6

---
+++ Picterest/CoreData/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  Picterest
+//
+//  Created by 신의연 on 2022/07/26.
+//
+
+import Foundation
---
+++ Picterest/DataManager/PicterestFileManager.swift
@@ -0,0 +1,12 @@
+//
+//  FileManager.swift
+//  Picterest
+//
+//  Created by 신의연 on 2022/07/26.
+//
+
+import Foundation
+
+class FileManager {
+    private let fileManager = 
+}
