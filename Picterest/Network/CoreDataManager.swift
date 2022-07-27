41b4cff0192a6099d688e2827940c3c068175af8
wooooozin <wooooozin@gmail.com>
2022-07-27 15:30:00 +0900

    [feat] CoreData 관련 기능 구현

--- Picterest/Network/CoreDataManager.swift
+++ Picterest/Network/CoreDataManager.swift
@@ -4,8 +4,46 @@
 //
 //
 
-import Foundation
+import UIKit
+import CoreData
 
 class CoreDataManager {
+    static let shared: CoreDataManager = CoreDataManager()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var coreData = [Picterest]()
     
+    func fetchCoreData() -> [Picterest] {
+        let fetchRequest: NSFetchRequest<Picterest> = Picterest.fetchRequest()
+        let context = appDelegate.persistentContainer.viewContext
+        do {
+            self.coreData = try context.fetch(fetchRequest)
+            return coreData
+        } catch {
+            print(error)
+            return []
+        }
+    }
+        
+    func saveCoreData(id: String, memo: String, url: String, location: String) {
+        let context = appDelegate.persistentContainer.viewContext
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Picterest", in: context) else { return }
+        guard let object = NSManagedObject(entity: entityDescription, insertInto: context) as? Picterest else { return }
+        
+        object.id = id
+        object.url = url
+        object.location = location
+        object.memo = memo
+        
+        appDelegate.saveContext()
+    }
+    
+    func deleteCoreData(ID: String) {
+        let context = appDelegate.persistentContainer.viewContext
+        let object = coreData.first {
+            $0.id == ID
+        }
+        guard let object = object else { return }
+        context.delete(object)
+        appDelegate.saveContext()
+    }
 }
