6be9b12be6f320597861be327e0acaeb7f125e36
신의연 <dusdl14@gmail.com>
2022-07-27 10:31:24 +0900

    [feat] coredata 모델과 CRD 매니저 구현 #6

--- Picterest/CoreData/CoreDataManager.swift
+++ Picterest/CoreData/CoreDataManager.swift
@@ -6,3 +6,75 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    static var shared = CoreDataManager()
+    
+    private init() {}
+    
+    var container: NSPersistentContainer?
+    
+    var mainContext: NSManagedObjectContext {
+        guard let context = container?.viewContext else {
+            fatalError()
+        }
+        return context
+    }
+    
+    func setup(modelName: String) {
+        container = NSPersistentContainer(name: modelName)
+        container?.loadPersistentStores(completionHandler: { (desc, error) in
+            if let error = error {
+                print(error)
+            }
+        })
+    }
+    
+    func createPictureData(id: String, memo: String, originUrl: String, localUrl: String) {
+        mainContext.perform {
+            
+            let newPicture = Picture(context: self.mainContext)
+            newPicture.id = id
+            newPicture.memo = memo
+            newPicture.originUrl = originUrl
+            newPicture.localUrl = localUrl
+            self.saveContext()
+        }
+    }
+    
+    func fetchSurvey() -> [Picture] {
+        var list = [Picture]()
+        mainContext.performAndWait {
+            let request: NSFetchRequest<Picture> = Picture.fetchRequest()
+            do {
+                list = try mainContext.fetch(request)
+            } catch {
+                print(error)
+            }
+        }
+        return list
+    }
+    
+    func delete(entity: Picture) {
+        mainContext.perform {
+            self.mainContext.delete(entity)
+            self.saveContext()
+        }
+    }
+    
+    func saveContext() {
+        mainContext.perform {
+            if self.mainContext.hasChanges {
+                do {
+                    try self.mainContext.save()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    
+}
