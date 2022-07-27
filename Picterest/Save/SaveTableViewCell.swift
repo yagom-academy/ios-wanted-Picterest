e75b29cad85518c3736752f91ff37cdb2b0dfbf0
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-07-27 13:15:53 +0900

    [feat] save cell 구현

--- Picterest/Save/SaveTableViewCell.swift
+++ Picterest/Save/SaveTableViewCell.swift
@@ -10,5 +10,40 @@ import UIKit
 class SaveTableViewCell: UITableViewCell {
     static let identifier = "SaveTableViewCell"
     
+    private let labelStackView = LabelStackView()
+    private let photoImageView: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = 10
+        image.clipsToBounds = true
+        
+        return image
+    }()
     
+    func fetchData() {
+        
+    }
+}
+
+extension SaveTableViewCell {
+    
+    func layout() {
+        [
+            photoImageView, labelStackView
+        ].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            photoImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            
+            labelStackView.topAnchor.constraint(equalTo: self.topAnchor),
+            labelStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            labelStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            labelStackView.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
 }
