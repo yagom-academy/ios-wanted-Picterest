53abf81300e35e849227c04bed2390be9eb61192
Kei <vvv.devk@gmail.com>
2022-07-27 10:52:09 +0900

    [feat] 이미지 캐싱 구현

--- Picterest/Extension/Picterest++UIImageView.swift
+++ Picterest/Extension/Picterest++UIImageView.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 extension UIImageView {
+    
     func loadImage(url: String) {
+        let key = NSString(string: url)
+        if let cachedImage = ImageCacheManager.shared.object(forKey: key) {
+            self.image = cachedImage
+            print("from cache")
+            return
+        }
+            
         guard let url = URL(string: url) else { return }
         URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
             guard let fetchedData = data, let image = UIImage(data: fetchedData), error == nil else { return }
+            ImageCacheManager.shared.setObject(image, forKey: key)
             DispatchQueue.main.async {
                 self?.image = image
             }
