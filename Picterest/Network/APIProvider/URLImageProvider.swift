90091bf6fbbbe15fc929525ebf504e312a86a402
bhoon-coding <bhooncoding@gmail.com>
2022-07-28 17:08:14 +0900

    [#7] feat: 이미지 캐시 처리

--- Picterest/Network/APIProvider/URLImageProvider.swift
+++ Picterest/Network/APIProvider/URLImageProvider.swift
@@ -19,18 +19,30 @@ class URLImageProvider: URLImageProviderType {
     
     private let networkRequester: NetworkRequesterType
     private var URLImageTask: URLSessionDataTask?
+    private let imageCache = NSCache<NSString, UIImage>()
     
     init(networkRequester: NetworkRequesterType) {
         self.networkRequester = networkRequester
     }
     
-    func fetchImage(from urlString: String, completion: @escaping (Result<UIImage, Error>) -> Void) {
+    func fetchImage(
+        from urlString: String,
+        completion: @escaping (Result<UIImage, Error>) -> Void
+    ) {
+        
+        let cacheKey = NSString(string: urlString)
+        if let cachedImage = imageCache.object(forKey: cacheKey) {
+            completion(.success(cachedImage))
+            return
+        }
+        
         URLImageTask = networkRequester.request(to: urlString, completion: { result in
             switch result {
             case .success(let data):
                 guard let image = UIImage(data: data) else {
                     return
                 }
+                self.imageCache.setObject(image, forKey: cacheKey)
                 completion(.success(image))
             case .failure(let error):
                 completion(.failure(error))
