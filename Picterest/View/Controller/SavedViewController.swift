ec1c4b5f8d7cb3aa66153af01472ce945a5f95f2
Kei <vvv.devk@gmail.com>
2022-07-28 22:14:21 +0900

    [feat] 두번째 화면 cell 구성

--- Picterest/View/Controller/SavedViewController.swift
+++ Picterest/View/Controller/SavedViewController.swift
@@ -8,5 +8,82 @@
 import UIKit
 
 class SavedViewController: UIViewController {
+    let viewModel = SavedCollectionViewModel()
     
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.reuseIdentifier)
+        collectionView.register(SavedCollectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SavedCollectionHeaderView.identifier)
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubviews()
+        viewModel.fetchSavedData()
+        viewModel.scrollViewUpdate = { [weak self] in
+            DispatchQueue.main.async {
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func configureSubviews() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
+
+extension SavedViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.savedImagesCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.reuseIdentifier, for: indexPath) as? ImageCollectionViewCell else { return UICollectionViewCell()}
+        let savedData = viewModel.imageAtIndex(indexPath.row)
+        let imageViewModel = ImageViewModel(image: savedData.image)
+        cell.configureCell(with: imageViewModel, index: indexPath.row)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionHeader,
+              let header = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: SavedCollectionHeaderView.identifier,
+                for: indexPath
+            ) as? SavedCollectionHeaderView
+        else { return UICollectionReusableView() }
+        header.configureView()
+        return header
+    }
+}
+
+extension SavedViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let savedImageViewModel = viewModel.imageAtIndex(indexPath.row)
+        let width: CGFloat = view.frame.width - 20
+        let imageRatio = CGFloat(savedImageViewModel.height) / CGFloat(savedImageViewModel.width)
+        let height = (width * imageRatio)
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0.5, left: 0, bottom: 0.5, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        CGSize(width: collectionView.frame.width - 32.0, height: 80.0)
+    }
 }
---
+++ Picterest/ViewModel/SavedCollectionViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  SavedCollectionViewModel.swift
+//  Picterest
+//
+//  Created by CHUBBY on 2022/07/28.
+//
+
+import UIKit
+import CloudKit
+
+class SavedCollectionViewModel {
+    private var savedImages: [SavedImageViewModel] = [] {
+        didSet {
+            scrollViewUpdate()
+        }
+    }
+    
+    var savedImagesCount: Int {
+        return savedImages.count
+    }
+    
+    var scrollViewUpdate: () -> Void = {}
+    
+    func fetchSavedData() {
+        let imageInfoData = CoreDataManager.shared.loadData()
+        imageInfoData.forEach {
+            guard let id = $0.id,
+                  let memo = $0.memo,
+                  let url = $0.url else { return }
+            let width = Int($0.width)
+            let height = Int($0.height)
+            let savedData = SavedImageViewModel(image: Image(id: id, width: width, height: height, urls: URLs(thumb: url)),
+                                                memo: memo)
+            savedImages.append(savedData)
+        }
+    }
+    
+    func imageAtIndex(_ index: Int) -> SavedImageViewModel {
+        return savedImages[index]
+    }
+}
+
+class SavedImageViewModel {
+    
+    var image: Image
+    var memo: String
+
+    init(image: Image, memo: String) {
+        self.image = image
+        self.memo = memo
+    }
+    
+    var width: Int {
+        return image.width
+    }
+    
+    var height: Int {
+        return image.height
+    }
+}
+
