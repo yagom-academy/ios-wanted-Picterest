5114d60aabfd37eb2423b6b107d8e323956c54d5
bhoon-coding <bhooncoding@gmail.com>
2022-07-28 20:41:41 +0900

    [#5] feat: 한페이지당 15개의 사진 배치 (cache값 확인후 다음 사진 받아와 추가로 나열)

--- Picterest/Utils/CustomCollectionViewLayout.swift
+++ Picterest/Utils/CustomCollectionViewLayout.swift
@@ -29,7 +29,16 @@ class CustomCollectionViewLayout: UICollectionViewLayout {
     }
     
     override func prepare() {
-        guard cache.isEmpty, let collectionView = collectionView else { return }
+        
+        guard let collectionView = collectionView else { return }
+        
+        let collectionViewItems = collectionView.numberOfItems(inSection: 0)
+        
+        if collectionViewItems == cache.count {
+            return
+        }
+        
+        cache.removeAll()
         
         let columnWidth = contentWidth / CGFloat(numberOfColums)
         var xOffset: [CGFloat] = []
