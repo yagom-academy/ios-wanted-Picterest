e9831eb7deb5cf7d2f099b123223df92da5dbb49
dahun-lee-daji <ssss6596@gmail.com>
2022-07-29 06:17:33 +0900

    feat: 탭바 컨트롤러 구현하여 기본 틀 구성
    Storyboard를 제거하여 코드기반 UI구성

---
+++ Picterest/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  Picterest
+//
+//  Created by 이다훈 on 2022/07/25.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+    
+}
