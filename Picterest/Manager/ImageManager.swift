94af26b8175ae708fcd59a6f01291c0c8420d522
bhoon-coding <bhooncoding@gmail.com>
2022-07-29 23:07:39 +0900

    [#6] feat:  좌 상단에 별 모양 표시로 다운로드 유무를 알 수 있게 구현

--- Picterest/Manager/ImageManager.swift
+++ Picterest/Manager/ImageManager.swift
@@ -15,36 +15,62 @@ class ImageManager {
     
     var fileManager = FileManager.default
     
-    func saveImage(id: String, image: UIImage) -> Bool {
-        
+    func getDirectoryURL() -> URL? {
         guard let documentURL = fileManager.urls(
             for: .documentDirectory,
             in: .userDomainMask
-        ).first else { return false }
+        ).first else { return nil }
+        
         let directoryURL = documentURL.appendingPathComponent("Photo")
         
-        do {
-            try fileManager.createDirectory(
-                at: directoryURL,
-                withIntermediateDirectories: false,
-                attributes: nil
-            )
-        } catch let error {
-            print("directroy 생성실패:\(error.localizedDescription)")
-        }
+        return directoryURL
+    }
+    
+    func saveImage(id: String, image: UIImage) -> Bool {
+        guard let directoryURL = getDirectoryURL() else { return false }
         
         guard let imageData = image.jpegData(compressionQuality: 1) else {
             return false
         }
 
         do {
-            try imageData.write(to: directoryURL.appendingPathComponent("\(id).jpeg"))
-            print("저장완료:\(directoryURL)")
+            try imageData.write(to: directoryURL.appendingPathComponent(id))
             return true
         } catch {
             print(error.localizedDescription)
             return false
         }
     }
+    
+//    func loadImageFromAlbum(folderName: String) -> [String] {
+//
+////        let nsDocumentDirectory = FileManager.SearchPathDirectory.documentDirectory
+////        let nsUserDomainMask = FileManager.SearchPathDomainMask.userDomainMask
+////         "/Users/bh/Library/Developer/CoreSimulator/Devices/B3D368A7-7088-46DA-BE24-221DBCD20A48/data/Containers/Data/Application/332743D0-6BB3-4767-8905-9DDF462742C2/Documents"
+//
+//// file:///Users/bh/Library/Developer/CoreSimulator/Devices/B3D368A7-7088-46DA-BE24-221DBCD20A48/data/Containers/Data/Application/B17065C3-99D6-4EE2-99A7-986C2B20153B/Documents/Photo/
+////        let paths = NSSearchPathForDirectoriesInDomains(nsDocumentDirectory, nsUserDomainMask, true)
+//
+////        let paths = try? fileManager.contentsOfDirectory(atPath: "Photo")
+//
+//        let paths = getDirectoryURL()
+//        let test = try! fileManager.contentsOfDirectory(atPath: paths)
+//        print("paths:\(test)")
+//
+////        var theitems: [String] = []
+////        if let dirPath = paths.first {
+////
+////
+////            let imageURL = URL(fileURLWithPath: dirPath).appendingPathComponent(folderName)
+////            do {
+////                theitems = try FileManager.default.contentsOfDirectory(atPath: imageURL.path)
+////                return theitems
+////            } catch let error {
+////                print(error.localizedDescription)
+////                return theitems
+////            }
+////        }
+//        return [""]
+//    }
     
 }
