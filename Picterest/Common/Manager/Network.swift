1b8dcd1a5912b54b75fcbfd04ee5e4ca9bb06ae3
prettyturtle <unifconti@gmail.com>
2022-07-30 16:16:25 +0900

    chore: API Key 변경

--- Picterest/Common/Manager/Network.swift
+++ Picterest/Common/Manager/Network.swift
@@ -18,7 +18,6 @@ enum NetworkError: String, Error {
 
 final class Network {
     private let page: Int
-    //    private let clientID = "7EoAQCEONksYSL9jv2U0iSUGUzpTffG6_YrGTdTXF2o"
     private let clientID = "jGA9GAwx92PWFeD_ptnML18QqLz5M3zpjGVEqXIKw7g"
     private let perPage = 15
     private let path = "https://api.unsplash.com/photos"
